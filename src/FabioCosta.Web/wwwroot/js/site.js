f9430a990b3830810acd78319aa45ab4e1500a13
Fabio Costa <fabio.costa89@gmail.com>
2021-05-21 22:07:06 +0200

    Add Back to Top button

--- src/FabioCosta.Web/wwwroot/js/site.js
+++ src/FabioCosta.Web/wwwroot/js/site.js
@@ -43,4 +43,19 @@
         }
     });
 
+    // Back to top button
+    var btn = $('#backTopButton');
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 300) {
+            btn.addClass('show');
+        } else {
+            btn.removeClass('show');
+        }
+    });
+
+    btn.on('click', function (e) {
+        e.preventDefault();
+        $('html, body').animate({ scrollTop: 0 }, '300');
+    });
 });
