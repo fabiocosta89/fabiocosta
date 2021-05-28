7f218adbef65fea1bcb653f1785703bde64bf6a1
Fabio Costa <fabio.Costa89@gmail.com>
2021-05-28 23:57:24 +0200

    Add SiteMap

---
+++ src/FabioCosta.Web/Controllers/SiteMapController.cs
@@ -0,0 +1,44 @@
+﻿namespace FabioCosta.Web.Controllers
+{
+    using FabioCosta.Web.Constants;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    using SimpleMvcSitemap;
+
+    using System;
+    using System.Collections.Generic;
+
+    public class SiteMapController : Controller
+    {
+        [ResponseCache(CacheProfileName = CacheConstants.Weekly)]
+        public IActionResult Index()
+        {
+            DateTime now = DateTime.UtcNow.ToLocalTime();
+
+            var nodes = new List<SitemapNode>
+            {
+                new SitemapNode(Url.Action("Index","Home"))
+                { 
+                    ChangeFrequency = ChangeFrequency.Weekly,
+                    LastModificationDate = now,
+                    Priority = 1.0M
+                },
+                new SitemapNode(Url.Action("Index","Privacy"))
+                {
+                    ChangeFrequency = ChangeFrequency.Yearly,
+                    LastModificationDate = now,
+                    Priority = 0.3M
+                },
+                new SitemapNode(Url.Action("Index","Version"))
+                {
+                    ChangeFrequency = ChangeFrequency.Weekly,
+                    LastModificationDate = now,
+                    Priority = 0.5M
+                }
+            };
+
+            return new SitemapProvider().CreateSitemap(new SitemapModel(nodes));
+        }
+    }
+}
