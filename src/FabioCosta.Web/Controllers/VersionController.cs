e0ad7f7e87b4a06058e1cb408df95a1d7f4d3ef7
Fabio Costa <fabio.Costa89@gmail.com>
2021-05-28 23:24:10 +0200

    Add logo to Nav Add version page

---
+++ src/FabioCosta.Web/Controllers/VersionController.cs
@@ -0,0 +1,15 @@
+﻿namespace FabioCosta.Web.Controllers
+{
+    using FabioCosta.Web.Constants;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    public class VersionController : Controller
+    {
+        [ResponseCache(CacheProfileName = CacheConstants.Weekly)]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/FabioCosta.Web/Helper/AssemblyHelper.cs
@@ -0,0 +1,28 @@
+﻿namespace FabioCosta.Web.Helper
+{
+    using System;
+    using System.Reflection;
+
+    public static class AssemblyHelper
+    {
+        /// <summary>
+        /// Retrives the assembly version 
+        /// </summary>
+        /// <returns>format v1.0</returns>
+        public static string GetAppVersion()
+        {
+            Version version = Assembly.GetEntryAssembly().GetName().Version;
+            return $"v{version.Major}.{version.Minor}";
+        }
+
+        /// <summary>
+        /// Retrives the assembly version with build
+        /// </summary>
+        /// <returns>format v1.0.0</returns>
+        public static string GetAppVersionWithBuild()
+        {
+            Version version = Assembly.GetEntryAssembly().GetName().Version;
+            return $"v{version.Major}.{version.Minor}.{version.Build}";
+        }
+    }
+}
